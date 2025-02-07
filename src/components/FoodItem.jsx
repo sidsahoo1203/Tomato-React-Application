4682628b2f20a4b15669182dba1d3efe09d2389c
sidsahoo1203 <sidsahoo2003@gmail.com>
2025-02-07 14:13:07 +0530

    Initial Commit

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { assets } from '../assets/assets';
-import { StoreContext } from '../context/storeContext';
+import { StoreContext } from '../context/StoreContext';
 
 const FoodItem = ({ id, name, price, description, image }) => {
   const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
